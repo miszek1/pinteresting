b9afeb6a403c7ab300c5ab30f0ba1f7c7ee17652
Afoster2 <Foster.andrew@rocketmail.com>
2015-05-16 08:46:56 -0500

    changed show.erb style, pin size

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -12,7 +12,7 @@ end
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :large => "1000x1000", :medium => "500x500>", :thumb => "300x300>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 
